693ac32be3fcb92223478972a1c774d4ea710438
Massimo Artizzu <massimo.artizzu@antreem.it>
2020-02-05 23:20:38 +0100

    Simplified support table ::parts

--- src/js/presentation.js
+++ src/js/presentation.js
@@ -4,7 +4,7 @@ customElements.define('support-table', class extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `<table part="support-frame"><caption part="support-caption"><slot></slot></caption><thead><tr></tr></thead><tbody><tr></tr></tbody></table>`;
+    this.shadowRoot.innerHTML = `<table part="frame"><caption part="caption"><slot></slot></caption><thead><tr></tr></thead><tbody><tr></tr></tbody></table>`;
     this.updateContent();
   }
 
@@ -15,13 +15,13 @@ customElements.define('support-table', class extends HTMLElement {
   updateContent() {
     this.shadowRoot.querySelector('thead > tr').innerHTML =
       this.constructor.observedAttributes.map(environment => {
-        return `<th part="support-header"><img part="support-icon" src="img/${environment}.svg" alt="${environment} logo"></th>`;
+        return `<th part="header"><img part="icon" src="img/${environment}.svg" alt="${environment} logo"></th>`;
       }).join('');
 
     this.shadowRoot.querySelector('tbody > tr').innerHTML =
       this.constructor.observedAttributes.map(environment => {
         const value = this.getAttribute(environment);
-        const partName = 'support-value ' + (value ? 'ok' : 'ko');
+        const partName = 'value ' + (value ? 'ok' : 'ko');
         return `<td part="${partName}">${value}</td>`;
       }).join('');
   }
